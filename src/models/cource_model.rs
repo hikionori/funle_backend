58e4ae33f15197b0ee2d7d14068945259904c2b6
Danila Verbinskiy <danv4656@gmail.com>
2022-11-16 19:43:18 +0200

    Add tests and cources models

--- src/models/cource_model.rs
+++ src/models/cource_model.rs
@@ -1,3 +1,18 @@
+use std::str::FromStr;
 
+use mongodb::bson::oid::ObjectId;
+use serde::{Deserialize, Serialize};
+
+use crate::models::tests_model::TestModel;
 
 // TODO: Create a course model
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CourseModel {
+    pub id: Option<ObjectId>,
+    pub name: String,
+    pub description: String,
+    pub tests: Vec<TestModel>,
+    // pub infos: Vec<InfoModel>,
+    // late add
+}
--- src/models/tests_model.rs
+++ src/models/tests_model.rs
@@ -1,2 +1,13 @@
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 
+use mongodb::bson::oid::ObjectId;
 // TODO: Create tests model
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TestModel {
+    pub id: Option<ObjectId>,
+    pub text_of_question: String,
+    pub answers: Vec<String>,
+    pub correct_answer: String,
+}
