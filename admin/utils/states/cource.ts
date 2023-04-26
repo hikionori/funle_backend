d1e23efb5db34f8d9e40c08f70832c3a72369a3d
Danila Verbinskiy <work.danv4656@gmail.com>
2023-04-26 16:58:12 +0300

    feat: create create.tsx, [id].tsx and cource.ts

---
+++ admin/utils/states/cource.ts
@@ -0,0 +1,40 @@
+import create from "zustand";
+
+/*
+    JSON:
+    {
+        _id: {
+            $oid: string
+        },
+        title: string,
+        description: string,
+        levels: [
+            [
+                level index: number,
+                [ array of nodes
+                    { node
+                        id: string,
+                        title: string,
+                        mini_image: string,
+                        type_ : string, // info | test
+                        n_of_tests: number | string, // if type_ == "test" else "None"
+                    }
+                ]
+            ]
+    }
+*/
+
+
+export const useCourseStore = create((set, get: any) => ({
+    _id: "",
+    title: "",
+    description: "",
+    levels: [],
+
+    // setters
+    setCourseId: (id: string) => set({ _id: id }),
+    setTitle: (title: string) => set({ title }),
+    setDescription: (description: string) => set({ description }),
+    setLevels: (levels: any[]) => set({ levels }),
+    
+}));
