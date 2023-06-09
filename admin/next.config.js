948bed5ef5411b23cb4757dd6ff6064abccc86e3
Danila Verbinskiy <work.danv4656@gmail.com>
2023-06-09 20:23:43 +0300

    update next.config.js

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    host: '0.0.0.0',
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+    host: '0.0.0.0',
+  },
 }
 
 module.exports = nextConfig
